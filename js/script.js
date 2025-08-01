4db5a9c7ce7368083bfdac50bf93ad42241143cb
MARCO <marcoorofino.22@gmail.com>
2025-08-01 16:54:41 +0200

    Commit Finale con Commenti

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+//Dichiarazione variabili generali, della rotazione della ruota, dell'alwaysWin e del resto
 root = document.documentElement;
-const alwaysWin = true;
+const alwaysWin = false;
 var i, ul, li = [];
 
+//Funzione per far girare la ruota
 function roll(){
+    //Controllo per verificare se abbia già girato o no
     if(document.getElementById("List").classList.contains("spin")){
         removeSpin();
         addSpin();
@@ -10,25 +13,31 @@ function roll(){
     else{
         addSpin();
     }
+
+    //Fix dallo standby
     document.getElementById("result").style.display = "none"; 
     document.getElementById("MyButton").disabled = true;
     document.getElementById("MyButton").classList.add("disabled");
     
+    //Dichiarazioni variabili per la ruota
     ul = document.getElementById("List");
     li = ul.getElementsByTagName('li');
 
+    //Controllo dell'alwaysWin
     if(alwaysWin == true) {
         i = 4;
         wheelSpin();
         document.getElementById("result").innerHTML = "🎉 2000€ 🎉";
     }
     else {
+        //Ottenimento del valore casuale
         var ul, txtValue;
         i = Math.floor(Math.random() * li.length);
         wheelSpin();
 
         txtValue = (li[i].getElementsByTagName("p")[0]).innerText;
 
+        //Output estetico personalizzato
         switch(txtValue){
             case "2000€":
                 document.getElementById("result").innerHTML = "🎉💰 2000€ 💰🎉";
@@ -45,6 +54,7 @@ function roll(){
     }
 }
 
+//Funzione per la rotazione della ruota
 function wheelSpin(){
     var indexCalc;
     indexCalc = (362 * 7) + 362 - (362 / ( li.length / ( i + 1) ));
@@ -52,20 +62,24 @@ function wheelSpin(){
     wheelWait();
 }
 
+//Attesa per la funzione inferiore a questa
 function wheelWait(){
     const waitAndReset = setTimeout(wheelReset, 10000);
 }
 
+//Funzione per lo sblocco del bottone e dell'ottenimento del risultato sotto
 function wheelReset(){
     document.getElementById("MyButton").classList.remove("disabled");
     document.getElementById("MyButton").disabled = false;
     document.getElementById("result").style.display = "block"; 
 }
 
+//Funzione per far partire l'animazione della ruota
 function addSpin(){
     document.getElementById("List").classList.add("spin");
 }
 
+//Funzione per far resettare la ruota per farla ripartire nell'ipotesi che sia stata fatta già girare
 function removeSpin(){
     document.getElementById("List").classList.remove("spin");
     void document.getElementById("List").offsetWidth;
